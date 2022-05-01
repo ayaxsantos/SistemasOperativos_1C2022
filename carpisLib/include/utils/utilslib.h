16243d60138c88035ab0cfccfd8c3084bace388e
ayaxsantos <ayax.asantos@gmail.com>
2022-04-30 23:23:33 -0300

    Fix error biblioteca utils falta de un prototipo en header

--- carpisLib/include/utils/utilslib.h
+++ carpisLib/include/utils/utilslib.h
@@ -7,6 +7,7 @@ void crear_buffer_funcion(t_funcion *funcion);
 void setear_funcion(t_funcion* funcion, void *valor);
 void *serializar_funcion(t_funcion* funcion, int bytes);
 void enviar_funcion(t_funcion* funcion, int socket_cliente);
+t_funcion* crear_funcion(func_code);
 void eliminar_funcion(t_funcion *funcion);
 
 int32_t recibir_direccion_memoria(int socket_cliente);
