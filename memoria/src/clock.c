ab7495b64785e047ff908cfbcc76bae2659d2fda
Alex Fernando Herbas Zambrana <fherbas@frba.utn.edu.ar>
2022-07-15 02:00:35 -0300

    Pruebas Checkpoint
    Refactor en el algoritmo clock

--- memoria/src/clock.c
+++ memoria/src/clock.c
@@ -6,7 +6,7 @@
  Si (Bit de uso == 1) ----> Bit de Uso = 0 y se apunta al siguiente marco y se vuelve a preguntar.
  */
 t_frame *realizar_algoritmo_clock(t_tabla_pagina *tabla_1n, t_col_pagina *registro,
-                                  accion accion_memoria, int32_t id_tabla_2n, int32_t entrada_tabla_2n) {
+                                  accion accion_memoria, int32_t entrada_tabla_1n, int32_t entrada_tabla_2n) {
 
 	t_frame *frame_a_asignar = NULL;
 	t_frame_asignado *posible_frame_a_reemplazar;
@@ -23,21 +23,23 @@ t_frame *realizar_algoritmo_clock(t_tabla_pagina *tabla_1n, t_col_pagina *regist
 			frame_a_asignar = NULL;
 			incrementar_puntero(tabla_1n);
 		} else { 					// BIT DE USO == 0
-			t_tabla_pagina *tabla_2n_pagina_victima = (t_tabla_pagina*)dictionary_get(tabla_1n->tabla, string_atoi(posible_frame_a_reemplazar->id_tabla_2n));
-			t_col_pagina *registro_pagina_victima = (t_col_pagina*)dictionary_get(tabla_2n_pagina_victima->tabla, string_atoi(posible_frame_a_reemplazar->entrada_tabla_2n));
+			t_tabla_pagina *tabla_2n_pagina_victima = (t_tabla_pagina*)dictionary_get(tabla_1n->tabla, string_itoa(posible_frame_a_reemplazar->entrada_tabla_1n));
+			t_col_pagina *registro_pagina_victima = (t_col_pagina*)dictionary_get(tabla_2n_pagina_victima->tabla, string_itoa(posible_frame_a_reemplazar->entrada_tabla_2n));
 
 			if(frame_a_asignar->modificado) {
-
-				// Realizar la escritura en SWAP, pagina victima
-
+                escribir_pagina_en_swap(tabla_1n->id_tabla,
+                                        calcular_nro_pagina(posible_frame_a_reemplazar->entrada_tabla_1n,
+                                                            posible_frame_a_reemplazar->entrada_tabla_2n),
+                                        frame_a_asignar->base);
 			}
-			// Realizar PAGE FAULT
+            realizar_page_fault(tabla_1n->id_tabla,
+                                calcular_nro_pagina(entrada_tabla_1n, entrada_tabla_2n),frame_a_asignar->base);
 
 			registro_pagina_victima->presencia = false;
 			registro->nro_frame = posible_frame_a_reemplazar->nro_frame;
 			registro->presencia = true;
 
-			posible_frame_a_reemplazar->id_tabla_2n = id_tabla_2n;
+			posible_frame_a_reemplazar->entrada_tabla_1n = entrada_tabla_1n;
 			posible_frame_a_reemplazar->entrada_tabla_2n = entrada_tabla_2n;
 
 			frame_a_asignar->modificado = accion_memoria;
